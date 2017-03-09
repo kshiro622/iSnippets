02468caac65a0f0951b7ce532b6e813097fd156f
Katherine Shiro <katie.shiro@gmail.com>
2017-03-09 16:00:16 -0600

    adds Snippets model to models.py

--- app/snippets/models.py
+++ app/snippets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=256, blank=False, null=False),
+    language = models.CharField(max_length=256, blank=False, null=False),
+    snippet = models.CharField(max_length=256, blank=False, null=False),
+    description = models.CharField(max_length=256, blank=False, null=False)
